f6887b007feba9df2cff21d113fe94dfb5fd6086
unic0rn9k <unic0rn9k@protonmail.com>
2022-01-12 15:37:26 +0100

    Pixels web

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
 //! Photon Emition Sumutaion
 
-fn main() {
-    println!("Hello, world!");
+use hyperpixel::*;
+use web_random::*;
+use web_timer::*;
+
+#[no_mangle]
+#[start]
+pub fn main() -> () {
+    let timer = Timer::default();
+    let mut random = Random::default();
+    let framebuffer = HyperPixel::new("#screen");
+    let (width, height) = framebuffer.dimensions();
+    let mut pixels = vec![0.0; width * height * 3];
+    timer.request_animation_loop(Box::new(move |_delta| {
+        for i in 0..pixels.len() {
+            pixels[i] = random.gen::<f32>() * 0.3;
+        }
+        framebuffer.render(&pixels)
+    }));
 }
