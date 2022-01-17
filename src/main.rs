9a78f07e661691e1d31ebb78db2b85a0b995b8ee
unic0rn9k <unic0rn9k@protonmail.com>
2022-01-17 14:24:27 +0100

    Added lines for electron orbits

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,17 @@ use winit::event_loop::{ControlFlow, EventLoop};
 use winit::window::WindowBuilder;
 use winit_input_helper::WinitInputHelper;
 
+const WIDTH: u32 = 320;
+const HEIGHT: u32 = 240;
+const SHELL_SPACE: f32 = 20.;
+
 lazy_static! {
     static ref ORBIT: ([f32; 255], [f32; 255]) = {
         let mut proton_x = [0f32; 255];
         let mut proton_y = [0f32; 255];
         for n in 0..255 {
-            proton_x[n] = (n as f32 / 255. * 16. * std::f32::consts::PI).sin() * 10.;
-            proton_y[n] = (n as f32 / 255. * 16. * std::f32::consts::PI).cos() * 10.;
+            proton_x[n] = (n as f32 / 255. * 16. * std::f32::consts::PI).sin() * SHELL_SPACE;
+            proton_y[n] = (n as f32 / 255. * 16. * std::f32::consts::PI).cos() * SHELL_SPACE;
         }
         (proton_x, proton_y)
     };
@@ -29,9 +33,6 @@ fn orbit_x(frame: u8, shell: f32) -> f32 {
 fn orbit_y(frame: u8, shell: f32) -> f32 {
     unsafe { HEIGHT as f32 / 2. + ORBIT.1.get_unchecked(frame as usize) * shell }
 }
-
-const WIDTH: u32 = 320;
-const HEIGHT: u32 = 240;
 
 /// Representation of the application state. In this example, a box will bounce around the screen.
 struct Particle {
@@ -145,35 +146,27 @@ async fn run() {
             .expect("Pixels error")
     };
 
-    let mut electrons = [Electron::new(1), Electron::new(2), Electron::new(4)];
-    let mut proton = Particle::new(WIDTH as i16 / 2, HEIGHT as i16 / 2);
-    proton.rgba = [0xff, 0xaa, 0x00, 0xff];
+    let mut electrons = [
+        Electron::new(1),
+        Electron::new(2),
+        Electron::new(3),
+        Electron::new(4),
+    ];
     let mut frame = 0u8;
 
     event_loop.run(move |event, _, control_flow| {
         // Draw the current frame
         if let Event::RedrawRequested(_) = event {
-            pixels.get_frame().iter_mut().for_each(|n| {
-                let mut n2 = *n;
-                n2 -= 2;
-                *n = n2 * (n2 < *n) as u8;
-            });
             //input.mouse().map(|(mx, my)| {
             //    //let (mx, my) = pixels.window_pos_to_pixel((mx, my)).unwrap_or((0, 0));
 
             //    //mouse.draw(pixels.get_frame());
             //});
 
-            proton.x = orbit_x(frame, 1.);
-            proton.y = orbit_y(frame, 1.);
             electrons.iter_mut().for_each(|e| e.update(frame));
             frame += 1;
 
-            if pixels
-                .render()
-                .map_err(|e| error!("pixels.render() failed: {}", e))
-                .is_err()
-            {
+            if pixels.render().is_err() {
                 *control_flow = ControlFlow::Exit;
                 return;
             }
@@ -192,7 +185,29 @@ async fn run() {
                 pixels.resize_surface(size.width, size.height);
             }
 
+            pixels
+                .get_frame()
+                .iter_mut()
+                .enumerate()
+                .for_each(|(n, p)| {
+                    let mut p2 = *p;
+                    p2 -= 4;
+                    *p = p2 * (p2 < *p) as u8;
+                    let x = (n / 4) % WIDTH as usize;
+                    let y = (n / 4 - x) / WIDTH as usize;
+                    let x = WIDTH as isize / 2 - x as isize;
+                    let y = HEIGHT as isize / 2 - y as isize;
+                    let delta = ((x * x + y * y) as f32).sqrt() as usize;
+
+                    if delta % SHELL_SPACE as usize == 0 && delta < 100 {
+                        *p = 70;
+                    }
+                });
+
+            let mut proton = Particle::new(WIDTH as i16 / 2, HEIGHT as i16 / 2);
+            proton.rgba = [0xff, 0xaa, 0x00, 0xff];
             proton.draw(pixels.get_frame());
+
             electrons.iter().for_each(|e| e.p.draw(pixels.get_frame()));
 
             // Update internal state and request a redraw
@@ -207,7 +222,7 @@ impl Particle {
         Self {
             x: x as f32,
             y: y as f32,
-            r: 10.,
+            r: 4.,
             dx: 10.,
             dy: 0.,
             rgba: [0, 1, 1, 255],
@@ -222,19 +237,14 @@ impl Particle {
         if self.y + self.r > HEIGHT as f32 || self.y - self.r < 0. {
             self.dy *= -1.;
         }
-        let mut dx = tx - self.x;
-        let mut dy = ty - self.y;
+        let dx = tx - self.x;
+        let dy = ty - self.y;
 
-        if dx.powi(2) + dy.powi(2) < self.r.powi(2) {
-            dx *= -5.;
-            dy *= -5.;
-        }
+        self.dx += dx * 0.05;
+        self.dy += dy * 0.05;
 
-        self.dx += dx * 0.03;
-        self.dy += dy * 0.03;
-
-        self.dx *= 0.94;
-        self.dy *= 0.94;
+        self.dx *= 0.8;
+        self.dy *= 0.8;
 
         self.x += self.dx;
         self.y += self.dy;
